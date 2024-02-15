0f945ab42897f027093a51bc1fe6616e99f9beac
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-02-15 17:28:00 +0300

    feat: add inter font and Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import styles from "./App.module.css";
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <h1>Привет, мир!</h1>
+    <div className={styles.main}>
+      <div className={styles.page}>
+        <Header />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.container}>
+      <div>Привет</div>
+      <div>Привет</div>
+      <div>Привет</div>
+    </header>
+  );
+};
+
+export default Header;
