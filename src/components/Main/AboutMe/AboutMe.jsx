7f0e6db37b513808fbc43d739f2fd99380174e65
Andrew Ruanov <rusanov.av@reliab.tech>
2024-02-20 17:53:46 +0300

    feat: add AboutMe layout (temp)

---
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -0,0 +1,17 @@
+import styles from "./AboutMe.module.css";
+
+const AboutMe = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Студент</h2>
+        <div>
+          <div></div>
+          <img />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
