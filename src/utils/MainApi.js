b6e4f3dd39f3eee9e99c574973da48c19bec8390
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-03-13 11:12:35 +0300

    hotfix

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.movies-diploma.nomoredomainsmonster.ru/';
+const BASE_URL = 'https://api.movies-diploma.nomoredomainsmonster.ru';
 
 class MainApi {
   constructor({ baseUrl }) {
