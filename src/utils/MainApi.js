bde0a28a805603fa898af2d7deb06737fe9f1376
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-03-13 10:46:32 +0300

    feat: add new base url

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.rusanov.nomoredomainsmonster.ru';
+const BASE_URL = 'https://api.movies-diploma.nomoredomainsmonster.ru/';
 
 class MainApi {
   constructor({ baseUrl }) {
